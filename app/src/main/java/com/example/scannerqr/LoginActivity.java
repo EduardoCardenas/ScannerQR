7695a87eb94e4a34d955e4f45b56a583413bd15d
EduardoCardenas <edgar.ojedaca@uanl.edu.mx>
2020-11-03 18:04:00 -0600

    Commit prueba de numero

--- app/src/main/java/com/example/scannerqr/LoginActivity.java
+++ app/src/main/java/com/example/scannerqr/LoginActivity.java
@@ -16,7 +16,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private int numero;
     private EditText etCorreo;
     private EditText etPassword;
 
